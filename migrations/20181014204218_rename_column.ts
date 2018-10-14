c2cf22135936e6b8c847ff796f436c0cd07caa90
Kittivara Rakthong <59050171@kmitl.ac.th>
2018-10-14 20:57:37 +0700

    return knex

--- migrations/20181014204218_rename_column.ts
+++ migrations/20181014204218_rename_column.ts
@@ -1,14 +1,14 @@
 import * as Knex from "knex";
 
 exports.up = (knex: Knex) => {
-    knex.schema.alterTable("Floor", (table) => {
+    return knex.schema.alterTable("Floor", (table) => {
         table.renameColumn("EntryPoint", "ToUpstair"),
         table.renameColumn("ExitPoint", "ToDownStair")
     });
 };
 
 exports.down = (knex: Knex) => {
-    knex.schema.alterTable("Floor", (table) => {
+    return knex.schema.alterTable("Floor", (table) => {
         table.renameColumn("ToUpstair", "EntryPoint"),
         table.renameColumn("ToDownStair", "ExitPoint")
     });
